bd7ae15c239e850a82844967c37c69755b53c632
taro katsumura <nayanderu@taronoMacBook-Air.local>
2025-11-21 01:09:45 +0900

    add: callback

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -11,4 +11,15 @@ class Quote < ApplicationRecord
   validates :body, presence: true, length: { maximum: 200 }
   validates :page,
             numericality: { only_integer: true, allow_nil: true, greater_than: 0 }
+
+  before_save :set_published_at_if_published
+
+  private
+
+  def set_published_at_if_published
+    # status が published に変わるタイミングで、published_at が空なら現在時刻を入れる
+    if will_save_change_to_status? && status_published? && published_at.nil?
+      self.published_at = Time.current
+    end
+  end
 end
