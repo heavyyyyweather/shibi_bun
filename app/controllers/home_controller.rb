2d1f7392e1706ed111c888f4fc14db1abf6cbab1
taro katsumura <nayanderu@taronoMacBook-Air.local>
2025-11-24 17:29:21 +0900

    add: see-more

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
+# app/controllers/home_controller.rb
 class HomeController < ApplicationController
   def index
     @quotes = Quote.published
                    .order(published_at: :desc)
+                   .includes(:book)
                    .page(params[:page])
                    .per(10)
+
+    respond_to do |format|
+      format.html  # 最初のページ読み込み
+      format.turbo_stream  # 「さらに表示」クリック時
+    end
   end
 end
