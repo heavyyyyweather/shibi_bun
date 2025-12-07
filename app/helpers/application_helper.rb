e7b06ac80423c5e6defa8462319679048ac7cd98
taro katsumura <nayanderu@taronoMacBook-Air.local>
2025-12-08 01:05:49 +0900

    fix header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 require Rails.root.join("lib/amazon_cover_helper")
 module ApplicationHelper
   include AmazonCoverHelper
+
+  def hide_new_quote_button?
+    controller_name.in?(%w[quotes books]) &&
+      action_name.in?(%w[new create edit update search])
+  end
 end
